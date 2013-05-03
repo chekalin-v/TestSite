8c3e4b41f47706836feb6e345ca0ca8ee5d599e2
# <#>
2013-05-03 13:31:42 +0600

    Async hello output

--- TestAssemblaProject/Program.cs
+++ TestAssemblaProject/Program.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestAssemblaProject
 {
     class Program
     {
+        CancellationTokenSource _cts = 
+            new CancellationTokenSource();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Assembla!");
+            Program p = new Program();
+
+            p.SayHello();
+
+            //Console.WriteLine();
+                        
+        }
+
+        private void SayHello()
+        {
+            var token =
+                _cts.Token;
+            
+            var task =
+                new Task(()=>OutputHello(token), token);            
+
+            task.Start();
+
             Console.ReadLine();
+            
+            _cts.Cancel();
+
+
+        }
+
+        private void OutputHello(CancellationToken token)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Hello, Assembla! Press any key to cancel...");
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(100);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                
+                Console.WriteLine("Canceled....");
+                
+            }
+            
         }
     }
 }
