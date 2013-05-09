317112bb044e73817ee34dbfc395ae676f9ebe03
Victor Chekalin <victor.chekalin@gmail.com>
2013-05-09 20:01:33 +0600

    Local changes test

--- TestAssemblaProject/Program.cs
+++ TestAssemblaProject/Program.cs
@@ -10,25 +10,31 @@ namespace TestAssemblaProject
     /// </summary>
     class Program
     {
-        readonly CancellationTokenSource _cts = 
+        readonly CancellationTokenSource _cts =
             new CancellationTokenSource();
 
         static void Main(int[] args)
         {
             /* Create a new instance of the program class*/
             var p = new Program();
+
             var r = new OtherClass();
+
             r.ShowSum();
-            p.SayHello();            
+            
+            p.SayHello();
         }
 
+        /// <summary>
+        /// Write hello to the console.
+        /// </summary>
         private void SayHello()
         {
             var token =
                 _cts.Token;
-            
+
             var task =
-                new Task(()=>OutputHello(token), token);
+                new Task(() => OutputHello(token), token);
 
             task.ContinueWith(t => SayGoodBye());
 
@@ -58,11 +64,11 @@ namespace TestAssemblaProject
             }
             catch (OperationCanceledException)
             {
-                
+
                 Console.WriteLine("Canceled....");
-                
+
             }
-            
+
         }
     }
 } //testing automerge
