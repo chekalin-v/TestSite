4cf56b5a38ad55f02a94ad969954d987228d5e99
Victor Chekalin <vchekalin@ecodomus.com>
2013-05-29 18:13:37 +0600

    Move category class to a separate file

--- TestAspNetApp/Model/Product.cs
+++ TestAspNetApp/Model/Product.cs
@@ -8,10 +8,4 @@
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }         
     }
-
-    internal class Category 
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
 }
