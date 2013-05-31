01d496203a58c88218bb9d72534af41fc75a676f
Victor Chekalin <chekalin-v@yandex.ru>
2013-05-31 14:37:40 +0600

    Add Configuration page. Ticket #04

---
+++ TestAspNetApp/SiteConfiguration.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestAspNetApp
+{
+    public partial class SiteConfiguration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AssemblyNameLabel.Text =
+                Assembly.GetExecutingAssembly().GetName().ToString();
+
+            VersionLabel.Text =
+                Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
